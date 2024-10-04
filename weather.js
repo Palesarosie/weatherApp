91c0b4c1649e376680e1d6bdcccecdabf8fc1261
Palesa <palesarosie4@gmail.com>
2024-10-05 01:35:52 +0200

    added api

--- weather.js
+++ weather.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement=document.querySelector("#temperature-unit");
+  let temp=response.data.temperature.current;
+  let cityElement=document.querySelector("#city-name")
+
+  cityElement.innerHTML=response.data.city;
+  temperatureElement.innerHTML=Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey="afd48f24t1f4b0392c26d754bfoe003c";
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${metric}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+  
+  searchCity(searchInput.value);
 }
 let searchSubmitForm = document.querySelector("#search-form");
 searchSubmitForm.addEventListener("submit", searchSubmit);
