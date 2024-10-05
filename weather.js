9136b022bf54103b6f384c78f00a53a5308c0571
Palesa <palesarosie4@gmail.com>
2024-10-06 00:57:55 +0200

    added more js

--- weather.js
+++ weather.js
@@ -1,10 +1,31 @@
 function refreshWeather(response) {
   let temperatureElement=document.querySelector("#temperature-unit");
   let temp=response.data.temperature.current;
-  let cityElement=document.querySelector("#city-name")
-
+  let cityElement=document.querySelector("#city-name");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windSpeedElement=document.querySelector("#wind-speed");
+  let timeElement=document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement=document.querySelector("#icon");
+  
+  iconElement.innerHTML=`<img src="${response.condition.icon_url}" class="weather-app-icon">`
+  descriptionElement.innerHTML=response.data.condition.description;
+  humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+  timeElement.innerHTML=formatDate(date);
   cityElement.innerHTML=response.data.city;
   temperatureElement.innerHTML=Math.round(temp);
+}
+function formatDate(date){
+  let minutes =date.getMinutes();
+  let hours =date.getHours();
+  let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let day =days[date.getDay()];
+
+  if (minutes <10) {
+    minutes =`0${minutes}`;
+  }
 }
 
 function searchCity(city) {
